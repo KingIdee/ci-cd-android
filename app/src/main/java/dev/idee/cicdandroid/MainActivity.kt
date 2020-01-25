718cc09e77a7fa5dba78d32b73e71de7180b50eb
Idorenyin Obong <idee4ril@gmail.com>
2020-01-25 18:57:09 +0100

    finish starter project

--- app/src/main/java/dev/idee/cicdandroid/MainActivity.kt
+++ app/src/main/java/dev/idee/cicdandroid/MainActivity.kt
@@ -2,38 +2,12 @@ package dev.idee.cicdandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: MainActivityViewModel
-    private val movieAdapter = MovieListAdapter(DiffUtilCallback())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupRecyclerView()
-        setupViewModel()
     }
 
-    private fun setupRecyclerView() {
-
-        with(movieRecyclerView) {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = movieAdapter
-        }
-    }
-
-    private fun setupViewModel() {
-        viewModel = ViewModelProviders.of(this)[MainActivityViewModel::class.java]
-        viewModel.movieListLiveData.observe(this, Observer {
-            progressBar.visibility = View.GONE
-            movieAdapter.submitList(it)
-        })
-
-    }
 }
